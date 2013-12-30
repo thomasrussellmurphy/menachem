8d07a896df7e78718f9110353990733d4f84b906
alphaq <snitrama@gmail.com>
2013-12-30 01:29:39 -0500

    There are doors now.

--- menachem.py
+++ menachem.py
@@ -2,17 +2,48 @@
 
 # Modules
 import sys
+import random
+
+# Constants
+MIN_DOORS = 2
+MAX_DOORS = 10
 
 # Loop for our interface
 def interface_loop():
-    print 'Testing Echo'
+    depth = 0
     while True:
-        input = raw_input('> ')
-        print input
+        n = random.randint(MIN_DOORS, MAX_DOORS)
+        print 'You are ' + str(depth) + ' rooms into the maze.'
+        print 'You are in a room. There are ' + str(n) + ' doors, zero-indexed.'
+        while True:
+            instr = raw_input('> ')
+            
+            if instr == '':
+                continue
+
+            elif instr == 'exit' or instr == 'quit':
+                print 'Are you sure?'
+                noinput = True
+                while noinput:
+                    response = raw_input('[y/n] ')
+                    if response == 'y':
+                        return
+                    elif response == 'n':
+                        noinput = False
+            
+            elif all([True if i in '1234567890' else False for i in instr]):
+                i = int(instr)
+                if 0 > i or n <= i:
+                    print 'Invalid input. Please enter a valid door number.'
+                else:
+                    print 'You go through door number ' + instr + '.'
+                    depth += 1
+                    break
+            else:
+                print 'Invalid input.'
 
 def main():
     if len(sys.argv) > 1:
-        print 'Hello World'
         interface_loop()
     else:
         print 'Hello World'
