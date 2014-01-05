f82616990777f78b131799fa42a10ce75afd587b
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2014-01-05 18:19:44 -0500

    Commented more on the planned structure of the localization class.

--- classes/localization.py
+++ classes/localization.py
@@ -1,4 +1,4 @@
-import json
+import json # Needed to parse saved localization file
 
 # TODO: Complete implementation and integrate into the State
 class Localization:
@@ -6,10 +6,13 @@ class Localization:
     def __init__(self):
         self.locale_name = ''
         self.locale_dict = {}
-        # Constant? self.locale_default
+        self.locale_default = 'en-US' # Constant?
 
     def set_locale(self, locale_name):
-        return
+        return False # Success of setting the new locale
     
     def get_phrases(self, phrase_key):
-        return ['']
+        return [''] # this pulls the string array out of the dict
+        
+    def get_string(self, phrase_key, components...):
+        return '' # We want to construct the full string with variable arguments
