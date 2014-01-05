a5864a5d290e5de0902fe1e317b8f58f79997716
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2014-01-05 13:07:39 -0500

    Created an initial skeleton of the localization class.

---
+++ classes/localization.py
@@ -0,0 +1,15 @@
+import json
+
+# TODO: Complete implementation and integrate into the State
+class Localization:
+
+    def __init__(self):
+        self.locale_name = ''
+        self.locale_dict = {}
+        # Constant? self.locale_default
+
+    def set_locale(self, locale_name):
+        return
+    
+    def get_phrases(self, phrase_key):
+        return ['']
