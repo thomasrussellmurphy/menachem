b418c13d6e435a451a1cd9a5477e4cde790e07f7
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2014-01-05 19:45:09 -0500

    Initial implemestation of Localization class

--- classes/localization.py
+++ classes/localization.py
@@ -12,17 +12,39 @@ class Localization:
 
     def set_locale(self, locale_name):
         # will need exception handling with these...
-        file = io.open('localization/' + locale_name + '/locale.json', 'r')
-        self.locale_dict = json.load(file)
+        try:
+            file = io.open('localization/' + locale_name + '/locale.json', 'r')
+        except (IOError) as err:
+            print 'ERROR: ' + err
+            return False
+            
+        try:
+            self.locale_dict = json.load(file)[0] # extract dict
+        except (ValueError) as err:
+            print 'ERROR: ' + err
+            return False
+        
         self.locale_name = locale_name
-        return False # Success of setting the new locale
+        return True # Success of setting the new locale
     
     def get_phrases(self, phrase_key):
-        # will need exception handling here, too
-        return self.locale_dict[phrase_key]
-        return [''] # this pulls the string array out of the dict
+        try:
+            return self.locale_dict[phrase_key]
+        except (KeyError) as err:
+            # fall back to default locale?
+            return ['that key was not found. have fun with this.']
         
     def get_string(self, phrase_key, components):
+        output = ''
         phrases = self.get_phrases(phrase_key)
-        # interleave phrases and components
-        return '' # We want to construct the full string with variable arguments
+        midstop = min(len(phrases),len(components))
+        
+        # interleave phrases and components        
+        for i in range(0, midstop):
+            output += phrases[i] + str(components[i])
+        for i in range(midstop, len(phrases)):
+            output += phrases[i]
+        for i in range(midstop, len(components)):
+            output += str(components[i])
+
+        return output
